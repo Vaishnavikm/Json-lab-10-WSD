cca082b8db3184965c4ece41872c38117ff3b9d5
Vaishnavikm <91455687+Vaishnavikm@users.noreply.github.com>
2022-02-07 14:23:38 +0530

    commit changes

--- FInd-My-Mechanic-JSON-main/products.js
+++ FInd-My-Mechanic-JSON-main/products.js
@@ -2,7 +2,7 @@ var myapp = angular.module('products', []);
       /*Dependency Injection*/
       myapp.controller('productctrl1',function($scope,$http)
 {
-   $http.get('https://vaishnavikm.github.io/WSD-lab-10-json/inventory.json')
+   $http.get('https://vaishnavikm.github.io/Json-lab-10-WSD/getproducts.json')
    .success(function(response)
 {
   $scope.names=response.inventory;
