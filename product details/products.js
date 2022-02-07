9505a7e6357c1610380d33d5c43b6926c1c6213c
Vaishnavikm <91455687+Vaishnavikm@users.noreply.github.com>
2022-02-07 14:38:57 +0530

    commit

--- product details/products.js
+++ product details/products.js
@@ -2,7 +2,7 @@ var myapp = angular.module('products', []);
       /*Dependency Injection*/
       myapp.controller('productctrl1',function($scope,$http)
 {
-   $http.get('https://vaishnavikm.github.io/json-file-/getproducts.json')
+   $http.get('https://vaishnavikm.github.io/Json-lab-10-WSD/product details/getproducts')
    .success(function(response)
 {
   $scope.names=response.inventory;
