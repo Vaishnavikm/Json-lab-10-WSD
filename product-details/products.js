ec07273ebe4358f71037efb622065bf49836895c
Vaishnavikm <91455687+Vaishnavikm@users.noreply.github.com>
2022-02-08 10:25:56 +0530

    fg

--- product-details/products.js
+++ product-details/products.js
@@ -1,12 +1,10 @@
-var myapp = angular.module('products', []);
-      /*Dependency Injection*/
-      myapp.controller('productctrl1',function($scope,$http)
-{
-   $http.get('https://vaishnavikm.github.io/Json-lab-10-WSD/product-details/getproducts.json')
-   .success(function(response)
-{
-  $scope.names=response.products;
-  $scope.rowlimit=8;
-}
-);
+var myapp = angular.module('productss', []);
+/*Dependency Injection*/
+myapp.controller('productctrl1', function ($scope, $http) {
+  $http.get('https://vaishnavikm.github.io/json-file-/getproducts.json')
+    .success(function (response) {
+      $scope.names = response.products;
+      $scope.rowlimit = 8;
+    }
+    );
 });
